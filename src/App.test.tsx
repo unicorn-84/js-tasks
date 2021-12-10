1da0bcbf537447ad7d95d79dc0b2c0b067c50362
unicorn-84 <unicorn-84@ya.ru>
2021-12-10 16:17:52 +0300

    Changed the test of the App component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,20 +2,7 @@ import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-jest.mock('./data/data.json', () => ({
-  __esModule: true,
-  projects: [
-    {
-      id: 1,
-      title: 'Project',
-      subtitle: '',
-      imageLink: '',
-      codeLink: '',
-      demoLink: '',
-      stateChartLink: '',
-    },
-  ],
-}));
+jest.mock('./data/data.json');
 
 describe('<App />', () => {
   test('should render correctly', () => {
