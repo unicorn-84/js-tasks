b6f2956a13cbf438bc3da6539ed9836ed92a1e6e
unicorn-84 <unicorn-84@ya.ru>
2021-12-10 15:41:01 +0300

    Changed the test of the LandingPage component

--- src/components/pages/LandingPage/LandingPage.test.tsx
+++ src/components/pages/LandingPage/LandingPage.test.tsx
@@ -2,9 +2,24 @@ import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 import LandingPage from '.';
 
+jest.mock('../../../data/data.json', () => ({
+  __esModule: true,
+  projects: [
+    {
+      id: 1,
+      title: 'Project',
+      subtitle: '',
+      imageLink: '',
+      codeLink: '',
+      demoLink: '',
+      stateChartLink: '',
+    },
+  ],
+}));
+
 describe('<LandingPage />', () => {
   test('should render correctly', () => {
     render(<LandingPage />);
-    expect(screen.getByText('Countdown')).toBeVisible();
+    expect(screen.getByText('Project')).toBeVisible();
   });
 });
