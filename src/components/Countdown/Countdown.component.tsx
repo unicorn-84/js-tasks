d4af03b385df75e16d6fa148ce391c7724e87576
unicorn-84 <unicorn-84@ya.ru>
2021-12-18 23:59:22 +0300

    Upgrade the Countdown component

--- src/components/Countdown/Countdown.component.tsx
+++ src/components/Countdown/Countdown.component.tsx
@@ -1,36 +1,33 @@
 import * as React from 'react';
-import { StyledBox, StyledTitle, StyledTime } from './Countdown.style';
+import {
+  StyledBox,
+  StyledTitle,
+  StyledTime,
+  StyledLevel,
+} from './Countdown.style';
 
 export interface ICountdownProps {}
 
 const Countdown: React.FC<ICountdownProps> = () => {
   return (
-    <nav className="level">
-      <div className="level-item has-text-centered">
-        <StyledBox>
-          <StyledTitle>Days</StyledTitle>
-          <StyledTime>365</StyledTime>
-        </StyledBox>
-      </div>
-      <div className="level-item has-text-centered">
-        <StyledBox>
-          <StyledTitle>Hours</StyledTitle>
-          <StyledTime>24</StyledTime>
-        </StyledBox>
-      </div>
-      <div className="level-item has-text-centered">
-        <StyledBox>
-          <StyledTitle>Minutes</StyledTitle>
-          <StyledTime>59</StyledTime>
-        </StyledBox>
-      </div>
-      <div className="level-item has-text-centered">
-        <StyledBox>
-          <StyledTitle>Seconds</StyledTitle>
-          <StyledTime>59</StyledTime>
-        </StyledBox>
-      </div>
-    </nav>
+    <StyledLevel>
+      <StyledBox>
+        <StyledTitle>Days</StyledTitle>
+        <StyledTime>365</StyledTime>
+      </StyledBox>
+      <StyledBox>
+        <StyledTitle>Hours</StyledTitle>
+        <StyledTime>24</StyledTime>
+      </StyledBox>
+      <StyledBox>
+        <StyledTitle>Minutes</StyledTitle>
+        <StyledTime>59</StyledTime>
+      </StyledBox>
+      <StyledBox>
+        <StyledTitle>Seconds</StyledTitle>
+        <StyledTime>59</StyledTime>
+      </StyledBox>
+    </StyledLevel>
   );
 };
 
--- src/components/Countdown/Countdown.style.ts
+++ src/components/Countdown/Countdown.style.ts
@@ -1,28 +1,61 @@
 import styled from 'styled-components';
+import { devices } from '../../utils';
+
+export const StyledLevel = styled.nav`
+  --size: 1rem;
+  --titleColor: rgb(141, 92, 17);
+  --timeColor: rgb(45, 24, 13);
+
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+
+  @media ${devices.mobileM} {
+    --size: 1.25rem;
+  }
+
+  @media ${devices.mobileL} {
+    --size: 1.5rem;
+  }
+
+  @media ${devices.tablet} {
+    flex-direction: row;
+    justify-content: space-around;
+  }
+
+  @media ${devices.laptopL} {
+    --size: 2rem;
+  }
+`;
 
 export const StyledBox = styled.div`
-  width: 200px;
-  height: 200px;
+  width: calc(var(--size) * 6);
+  height: calc(var(--size) * 6);
   background-color: rgba(255, 254, 202, 0.7);
-  border-radius: 12px;
+  border-radius: calc(var(--size) / 3);
   display: flex;
   flex-flow: column nowrap;
   align-items: center;
   justify-content: center;
+  margin-top: 2.5vh;
+  margin-bottom: 2.5vh;
+
+  @media ${devices.tablet} {
+    margin-top: 0;
+    margin-bottom: 0;
+  }
 `;
 
 export const StyledTitle = styled.div`
-  font-size: 1.5rem;
+  font-size: calc(var(--size) / 1.75);
   text-transform: uppercase;
   font-weight: 500;
-  color: rgb(141, 92, 17);
-  /* margin-bottom: 0.5rem; */
+  color: var(--titleColor);
 `;
 
 export const StyledTime = styled.div`
-  font-size: 5.5rem;
+  font-size: calc(var(--size) * 2);
   text-transform: uppercase;
   font-weight: 700;
-  color: #2d180d;
-  line-height: 1;
+  color: var(--timeColor);
 `;
