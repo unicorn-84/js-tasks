ed14bc8e6faf31cc2f403d2e21b9c29517eb0f8d
unicorn-84 <unicorn-84@ya.ru>
2021-12-19 21:57:39 +0300

    Changed images for the CountdownPage component

--- src/components/pages/CountdownPage/CountdownPage.style.ts
+++ src/components/pages/CountdownPage/CountdownPage.style.ts
@@ -5,16 +5,23 @@ import { devices } from '../../../utils';
 const StyledCountdownPage = styled.section<ICountdownPageProps>`
   background: transparent no-repeat;
   background-size: cover;
-  background-image: url('https://res.cloudinary.com/unicorn84/image/upload/c_scale,q_auto,w_768/v1639772684/js-tasks/countdown/happy-new-year-2022-tablet.webp');
+  background-image: url('https://res.cloudinary.com/unicorn84/image/upload/c_scale,q_auto,w_320/v1639940064/js-tasks/countdown/new-year.webp');
   background-position: center;
 
-  @media ${devices.tablet} {
-    background-image: url('https://res.cloudinary.com/unicorn84/image/upload/c_scale,q_auto,w_1024/v1639772760/js-tasks/countdown/happy-new-year-2022-laptop.webp');
-    background-position: left;
+  @media ${devices.mobileL} {
+    background-image: url('https://res.cloudinary.com/unicorn84/image/upload/c_scale,q_auto,w_425/v1639939906/js-tasks/countdown/new-year-mobileL.webp');
   }
-  @media ${devices.laptop} {
-    background-position: left center;
-    background-image: url('https://res.cloudinary.com/unicorn84/image/upload/c_scale,q_auto,w_1920/v1639772786/js-tasks/countdown/happy-new-year-2022-desktop.webp');
+
+  @media ${devices.tablet} {
+    background-image: url('https://res.cloudinary.com/unicorn84/image/upload/c_scale,q_auto,w_768/v1639939527/js-tasks/countdown/new-year-tablet.webp');
+  }
+
+  @media ${devices.laptopL} {
+    background-image: url('https://res.cloudinary.com/unicorn84/image/upload/c_scale,q_auto,w_1440/v1639939329/js-tasks/countdown/new-year-laptopL.webp');
+  }
+
+  @media ${devices.desktop} {
+    background-image: url('https://res.cloudinary.com/unicorn84/image/upload/c_scale,q_auto,w_2560/v1639938644/js-tasks/countdown/new-year-desktop.webp');
   }
 `;
 
